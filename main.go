9d9341e6e8cdeb6bba36beda52e89199d36aeb65
u1and0 <e01.ando60@gmail.com>
2020-07-17 19:34:45 +0900

    [mod] get first, last row at second column

--- main.go
+++ main.go
@@ -17,9 +17,11 @@ $ elen -f -75 -f 205-280 -f 425-575 -f 725-800 -f 925- \
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"os"
 	"strings"
 )
@@ -59,12 +61,61 @@ func main() {
 		return // Exit with version info
 	}
 
-	b, err := ioutil.ReadFile("data/20200627_180505.txt")
+	// b, err := ioutil.ReadFile("data/20200627_180505.txt")
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "cannot read a file: %s", err)
+	// 	os.Exit(1)
+	// }
+
+	filename := "data/20200627_180505.txt"
+	file, err := os.Open(filename)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "cannot read a file: %s", err)
-		os.Exit(1)
+		fmt.Println(err)
 	}
-	lines := string(b)
-	fmt.Println(lines)
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	var (
+		content []string
+		config  string
+		isConf  = true
+	)
+	for {
+		line, isPrefix, err := reader.ReadLine()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println(err)
+		}
+		if isConf {
+			config = string(line)
+			isConf = false
+			continue
+		}
+		s := string(bytes.TrimSpace(line))
+		ss := strings.Replace(s, " ", ",", 1) // Trim whitespace, Middle space=>,
+		sss := strings.Split(ss, ",")         // split delim ,
+		ssss := strings.TrimSpace(sss[1])     // Get second column
+		content = append(content, ssss)
+		if !isPrefix {
+			fmt.Println()
+		}
+	}
+	content = content[:len(content)-1]
+	fmt.Println(config)
+	fmt.Println(content[0], content[len(content)-1])
+	// c := content[1 : len(content)-1]
+	// fmt.Println(c)
+
+	// lines := string(b)
+	// lines := strings.Split(string(b), "\n")
+	// for _, l := range lines {
+	// 	fmt.Println(l)
+	// }
+	// st := strings.SplitN(string(b), "\n", 1)
+	// df := dataframe.ReadCSV(content, delimiter(`\t`))
+	// fmt.Println(st[0])
 	fmt.Println(field)
 }
